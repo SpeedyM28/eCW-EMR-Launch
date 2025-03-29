7fe94fea02ecd88bb78c02ba86b402c120d9f0c9
Jamil Bitar <jamil.bitarm76@gmail.com>
2025-03-28 22:08:38 -0400

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LaunchPage from "./LaunchPage";
 import CallbackPage from "./CallbackPage";
 
